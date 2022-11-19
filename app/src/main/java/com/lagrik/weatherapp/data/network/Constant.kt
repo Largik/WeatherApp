3f175478f98e8720328045318430ae9c5a9ee967
Владимир Афанасьев <vladimir.afanasev@irlix.ru>
2022-11-19 17:24:13 +0400

    changed count days

--- app/src/main/java/com/lagrik/weatherapp/data/network/Constant.kt
+++ app/src/main/java/com/lagrik/weatherapp/data/network/Constant.kt
@@ -3,7 +3,7 @@ package com.lagrik.weatherapp.data.network
 object Constant {
     const val BASE_URL = "https://api.weatherapi.com/v1/"
     const val API_KEY = "efdffc1f23084891b5975219221711"
-    const val COUNT_DAYS = 3
+    const val COUNT_DAYS = 4
     const val CELSIUS = "°C"
     const val HUMIDITY = "Humidity: "
     const val WIND = " kph"
